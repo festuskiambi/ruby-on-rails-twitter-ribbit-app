1d32255e6df967d11ab8b57fcb6c82f44123d3ad
kiamfestoh@gmail <kiamfestoh@gmail.com>
2015-04-25 16:43:21 +0300

    user form and profile pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,21 @@
 class UsersController < ApplicationController
 def new
-	@user = user.new
+	 @user = User.new
 end
 def create
-  @user = User.new(params[:user])
+  @user = User.new(user_params)
  
   if @user.save
     redirect_to @user, notice: "Thank you for signing up for Ribbit!"
   else
     render 'new'
   end
+  def user_params
+
+  end 
 end
-def show
-  @user = User.find(params[:id])
-end
+def user_params
+      params.require(:user).permit(:avatar_url,:name,:username, :email, :password, :password_confirmation)
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,21 @@ class User < ActiveRecord::Base
 before_save :create_avatar_url
 before_validation :prep_email
 has_secure_password
-attr_accessible :avatar_url, :email, :name, :password, :password_confirmation, :username
+def user_params
+      params.require(:user).permit(:avatar_url,:name,:username, :email, :password, :password_confirmation)
+    end
+
 validates :name, presence: true
 validates :username, uniqueness: true, presence: true
-validates :email, uniqueness: true, presence: true, format: { with: /^[\w.+-]+@([\w]+.)+\w+$/ }
+validates :email, uniqueness: true, presence: true, format: { with: /\A[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\z/i }
+
+def create_avatar_url
+    self.avatar_url = "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(self.email)}?s=50"
+end
 private
  
 def prep_email
     self.email = self.email.strip.downcase if self.email
 end
-def create_avatar_url
-    self.avatar_url = "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(self.email)}?s=50"
-end
+
 end
