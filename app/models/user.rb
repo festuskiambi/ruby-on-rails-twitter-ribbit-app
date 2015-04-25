a00b184e9db4df66a918d1d8147a69d070655604
kiamfestoh@gmail <kiamfestoh@gmail.com>
2015-04-25 09:37:31 +0300

    added validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ActiveRecord::Base
+has_secure_password
+attr_accessible :avatar_url, :email, :name, :password, :password_confirmation, :username
+validates :name, presence: true
+validates :username, uniqueness: true, presence: true
+validates :email, uniqueness: true, presence: true, format: { with: /^[\w.+-]+@([\w]+.)+\w+$/ }
+private
+ 
+def prep_email
+    self.email = self.email.strip.downcase if self.email
+end
 end
