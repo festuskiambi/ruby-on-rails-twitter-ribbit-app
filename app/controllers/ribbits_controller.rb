0724ad941d04c32a5e3122ed08ddbd70d534ec2e
kiamfestoh@gmail <kiamfestoh@gmail.com>
2015-04-26 15:02:52 +0300

    created ribbit functionality

--- app/controllers/ribbits_controller.rb
+++ app/controllers/ribbits_controller.rb
@@ -1,2 +1,18 @@
 class RibbitsController < ApplicationController
+	def index
+    @ribbits = Ribbit.all include: :user
+    @ribbit = Ribbit.new
+
+end
+def create
+  @ribbit = Ribbit.new(params[:ribbit])
+  @ribbit.userid = current_user.id</p>
+ 
+  if @ribbit.save
+      redirect_to current_user 
+  else
+      flash[:error] = "Problem!"
+      redirect_to current_user
+  end
+end
 end
